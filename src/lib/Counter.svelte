9981b8725359ed2a3f0aaac3c35753cf3dde6d59
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-26 07:06:00 +0900

    style: main wrapper

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,51 +1,50 @@
 <script lang="ts">
-  let editing = false;
-  let title = '';
-  export let count: number;
+    let editing = false;
+    let title = "";
+    export let count: number;
 
-  function toggleEdit() {
-    editing =!editing;
-  }
+    function toggleEdit() {
+        editing = !editing;
+    }
 
-  function increment() {
-    count += 1;
-  }
+    function increment() {
+        count += 1;
+    }
 
-  function decrement() {
-    count -= 1;
-  }
+    function decrement() {
+        count -= 1;
+    }
 
-  function reset() {
-    count = 0;
-  }
+    function reset() {
+        count = 0;
+    }
 </script>
 
-{#if editing}
-  <input 
-    bind:value={title}
-    type='text'
-    placeholder='Title'
-    on:blur={toggleEdit}
-  />
-{:else}
-  <span>{title ? title : "Click edit to add title"}</span>
-{/if}
-<button on:click={toggleEdit}>
-  {#if editing}
-    Cancel
-  {:else}
-    Edit
-  {/if}
-</button>
-<slot />
-
-<button on:click={increment}>
-  Increment
-</button>
-<span>Count is {count}</span>
-<button on:click={decrement}>
-  Decrement
-</button>
-<button on:click={reset}>
-  Reset
-</button>
+<div class='my-2 lg:m-5 items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-4xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-2'>
+    <div>
+        {#if editing}
+          <input
+              bind:value={title}
+              type="text"
+              placeholder="Title"
+              on:blur={toggleEdit}
+          />
+        {:else}
+          <span>{title ? title : "Click edit to add title"}</span>
+        {/if}
+        <button on:click={toggleEdit}>
+          {#if editing}
+              Cancel
+          {:else}
+              Edit
+          {/if}
+        </button>
+        <slot />
+    </div>
+    <div class='p-2'>
+      <button on:click={increment}> Increment </button>
+      <span>Count is {count}</span>
+      <button on:click={decrement}> Decrement </button>
+      <button on:click={reset}> Reset </button>
+    </div>
+</div>
