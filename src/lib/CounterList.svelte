3c5af87d07793c45430d381476207ea82627f643
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-26 01:34:16 +0900

    feat: sum & count state

--- src/lib/CounterList.svelte
+++ src/lib/CounterList.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import Counter from "./Counter.svelte";
 
-    let counters: Array<{id: string}> = [ {id: '1'} ];
+    let counters: Array<{id: string, count: number}> = [ {id: '1', count: 0} ];
+
+    $: sum = counters.map(el => el.count).reduce((sum, value) => sum + value, 0);
 
     function addCounter() {
-        counters = [...counters, { id: String(counters.length + 1) }]
+        counters = [...counters, { id: String(counters.length + 1), count: 0 }]
     }
 
     function removeCounterWithId(id: string) {
@@ -17,10 +19,11 @@
     <ul>
     {#each counters as counter (counter.id)}
         <li>
-            <Counter />
+            <Counter bind:count={counter.count}/>
             <button on:click={() => removeCounterWithId(counter.id)}>Remove</button>
         </li>
     {/each}
     </ul>
     <button on:click={addCounter}>Add Counter</button>
+    <span>The sum is {sum}</span>
 </div>
