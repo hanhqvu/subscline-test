748e01e127c1bfd586624162caf09fa1909217fd
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-26 07:24:36 +0900

    chore: formatting

--- src/test/Counter.test.ts
+++ src/test/Counter.test.ts
@@ -1,54 +1,54 @@
-import { describe, test, expect, beforeEach } from 'vitest'
-import { render, screen } from '@testing-library/svelte';
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-import Counter from '../lib/Counter.svelte';
+import { describe, test, expect, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import Counter from "../lib/Counter.svelte";
 
 const user = userEvent.setup();
 
 beforeEach(() => {
-    render(Counter, { count: 0 })
-})
+  render(Counter, { count: 0 });
+});
 
-test('should render correctly', () => {
-    expect(screen.getByText(/count/)).toBeInTheDocument();
-    expect(screen.getByText(/click edit/i)).toBeInTheDocument();
-})
+test("should render correctly", () => {
+  expect(screen.getByText(/count/)).toBeInTheDocument();
+  expect(screen.getByText(/click edit/i)).toBeInTheDocument();
+});
 
-test('should be able to increment', async () => {
-    await user.click(screen.getByText(/increment/i));
+test("should be able to increment", async () => {
+  await user.click(screen.getByText(/increment/i));
 
-    expect(screen.getByText(/count/i).innerHTML).toBe('Count is 1');
-})
+  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 1");
+});
 
-test('should be able to decrement', async () => {
-    await user.click(screen.getByText(/decrement/i));
+test("should be able to decrement", async () => {
+  await user.click(screen.getByText(/decrement/i));
 
-    expect(screen.getByText(/count/i).innerHTML).toBe('Count is -1');
-})
+  expect(screen.getByText(/count/i).innerHTML).toBe("Count is -1");
+});
 
-test('should be able to reset', async () => {
-    await user.click(screen.getByText(/increment/i));
-    await user.click(screen.getByText(/increment/i));
+test("should be able to reset", async () => {
+  await user.click(screen.getByText(/increment/i));
+  await user.click(screen.getByText(/increment/i));
 
-    expect(screen.getByText(/count/i).innerHTML).toBe('Count is 2');
+  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 2");
 
-    await user.click(screen.getByText(/reset/i));
+  await user.click(screen.getByText(/reset/i));
 
-    expect(screen.getByText(/count/i).innerHTML).toBe('Count is 0');
-})
+  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 0");
+});
 
-test('should be able to edit title', async () => {
-    await user.click(screen.getByText('Edit'));
-    await user.type(screen.getByPlaceholderText('Title'), 'New Title');
-    await user.click(screen.getByText(/reset/i));
+test("should be able to edit title", async () => {
+  await user.click(screen.getByText("Edit"));
+  await user.type(screen.getByPlaceholderText("Title"), "New Title");
+  await user.click(screen.getByText(/reset/i));
 
-    expect(screen.getByText('New Title')).toBeInTheDocument();
+  expect(screen.getByText("New Title")).toBeInTheDocument();
 
-    await user.click(screen.getByText('Edit'));
-    await user.clear(screen.getByPlaceholderText('Title'));
-    await user.type(screen.getByPlaceholderText('Title'), 'Another Title');
-    await user.click(screen.getByText(/reset/i));
+  await user.click(screen.getByText("Edit"));
+  await user.clear(screen.getByPlaceholderText("Title"));
+  await user.type(screen.getByPlaceholderText("Title"), "Another Title");
+  await user.click(screen.getByText(/reset/i));
 
-    expect(screen.getByText('Another Title')).toBeInTheDocument();
-})
+  expect(screen.getByText("Another Title")).toBeInTheDocument();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   test: {
     globals: true,
-    environment: 'jsdom',
-  }
-})
+    environment: "jsdom",
+  },
+});
