9641965c1e08f8451f19b5797beed345cba9a6fa
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-26 01:33:43 +0900

    feat: add test for sum

--- src/test/CounterList.test.ts
+++ src/test/CounterList.test.ts
@@ -15,18 +15,56 @@ test('should render correctly', () => {
     expect(screen.getByText(/add counter/i)).toBeInTheDocument();    
 })
 
-test('should be able to add counter', async () => {
-    await user.click(screen.getByText(/add counter/i));
-
-    expect(screen.getAllByText(/count is/).length).toBe(2);
+describe('should handle adding and removing counter', () => {
+    test('should be able to add counter', async () => {
+        await user.click(screen.getByText(/add counter/i));
+    
+        expect(screen.getAllByText(/count is/).length).toBe(2);
+    })
+    
+    test('should be able to remove counter', async () => {
+        await user.click(screen.getByText(/add counter/i));
+        expect(screen.getAllByText(/count is/).length).toBe(2);
+    
+        await user.click(screen.getAllByText(/remove/i)[0]);
+        expect(screen.getAllByText(/count is/).length).toBe(1);
+    })
 })
 
-test('should be able to remove counter', async () => {
-    await user.click(screen.getByText(/add counter/i));
+describe('should handle sum', () => {
+    test('sum should increase & decrease when 1 count changes', async () => {
+        await user.click(screen.getAllByText(/increment/i)[0]);
+        expect(screen.getByText('The sum is 1')).toBeInTheDocument();
 
-    expect(screen.getAllByText(/count is/).length).toBe(2);
+        await user.click(screen.getAllByText(/decrement/i)[0]);
+        expect(screen.getByText('The sum is 0')).toBeInTheDocument();
 
-    await user.click(screen.getAllByText(/remove/i)[0]);
+        await user.click(screen.getAllByText(/increment/i)[0]);
+        await user.click(screen.getAllByText(/reset/i)[0]);
+        expect(screen.getByText('The sum is 0')).toBeInTheDocument();
+    })
 
-    expect(screen.getAllByText(/count is/).length).toBe(1);
+    test('sum should increase & decrease when multiple counts changes', async () => {
+        await user.click(screen.getByText(/add counter/i));
+        
+        await user.click(screen.getAllByText(/increment/i)[0]);
+        await user.click(screen.getAllByText(/decrement/i)[1]);
+        expect(screen.getByText('The sum is 0')).toBeInTheDocument();
+
+        await user.click(screen.getAllByText(/decrement/i)[0]);
+        await user.click(screen.getAllByText(/reset/i)[1]);
+        expect(screen.getByText('The sum is 0')).toBeInTheDocument();
+    })
+
+    test('sum should increase & decrease when adding & removing counters', async () => {
+        await user.click(screen.getByText(/add counter/i));
+        await user.click(screen.getAllByText(/increment/i)[0]);
+        expect(screen.getByText('The sum is 1')).toBeInTheDocument();
+
+        await user.click(screen.getAllByText(/decrement/i)[1]);
+        expect(screen.getByText('The sum is 0')).toBeInTheDocument();
+
+        await user.click(screen.getAllByText(/remove/i)[0]);
+        expect(screen.getByText('The sum is -1')).toBeInTheDocument();
+    })
 })
