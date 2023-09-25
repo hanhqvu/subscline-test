443edb0954594150450f885c9a43fa56e3d1de3c
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-25 14:34:56 +0900

    feat: counter list impl

---
+++ src/lib/CounterList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Counter from "./Counter.svelte";
+
+    let counters: Array<{id: string}> = [ {id: '1'} ];
+
+    function addCounter() {
+        counters = [...counters, { id: String(counters.length + 1) }]
+    }
+
+    function removeCounterWithId(id: string) {
+        counters = counters.filter(el => el.id !== id);
+    }
+</script>
+
+
+<div>
+    <ul>
+    {#each counters as counter (counter.id)}
+        <li>
+            <Counter />
+            <button on:click={() => removeCounterWithId(counter.id)}>Remove</button>
+        </li>
+    {/each}
+    </ul>
+    <button on:click={addCounter}>Add Counter</button>
+</div>
