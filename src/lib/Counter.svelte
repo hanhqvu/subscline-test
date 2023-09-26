8363419bdd1ecfd6a15551681b8025f3a17e7733
hanhqvu <hanhqvu@users.noreply.github.com>
2023-09-27 00:10:50 +0900

    chore: formatting

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,63 +1,65 @@
 <script lang="ts">
-    let editing = false;
-    let title = "";
-    export let count: number;
+  let editing = false;
+  let title = '';
+  export let count: number;
 
-    function handleKeyDown(e: KeyboardEvent) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            toggleEdit();
-        }
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      toggleEdit();
     }
+  }
 
-    function toggleEdit() {
-        editing = !editing;
-    }
+  function toggleEdit() {
+    editing = !editing;
+  }
 
-    function cancelEdit() {
-        title = '';
-        toggleEdit()
-    }
+  function cancelEdit() {
+    title = '';
+    toggleEdit();
+  }
 
-    function increment() {
-        count += 1;
-    }
+  function increment() {
+    count += 1;
+  }
 
-    function decrement() {
-        count -= 1;
-    }
+  function decrement() {
+    count -= 1;
+  }
 
-    function reset() {
-        count = 0;
-    }
+  function reset() {
+    count = 0;
+  }
 </script>
 
-<div class='my-2 lg:m-5 items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-4x dark:border-gray-700 dark:bg-gray-800 p-2'>
-    <div>
-        {#if editing}
-          <input
-              bind:value={title}
-              type="text"
-              placeholder="Title"
-              on:blur={toggleEdit}
-              on:keydown={handleKeyDown}
-          />
-        {:else}
-          <span>{title ? title : "Click edit to add title"}</span>
-        {/if}
-        <button on:click={cancelEdit}>
-          {#if editing}
-              Cancel
-          {:else}
-              Edit
-          {/if}
-        </button>
-        <slot />
-    </div>
-    <div class='p-2'>
-      <span>Count is {count}</span>
-      <button on:click={increment}> Increment </button>  
-      <button on:click={decrement}> Decrement </button>
-      <button on:click={reset}> Reset </button>
-    </div>
+<div
+  class="my-2 lg:m-5 items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-4x dark:border-gray-700 dark:bg-gray-800 p-2"
+>
+  <div>
+    {#if editing}
+      <input
+        bind:value={title}
+        type="text"
+        placeholder="Title"
+        on:blur={toggleEdit}
+        on:keydown={handleKeyDown}
+      />
+    {:else}
+      <span>{title ? title : 'Click edit to add title'}</span>
+    {/if}
+    <button on:click={cancelEdit}>
+      {#if editing}
+        Cancel
+      {:else}
+        Edit
+      {/if}
+    </button>
+    <slot />
+  </div>
+  <div class="p-2">
+    <span>Count is {count}</span>
+    <button on:click={increment}> Increment </button>
+    <button on:click={decrement}> Decrement </button>
+    <button on:click={reset}> Reset </button>
+  </div>
 </div>
--- src/test/Counter.test.ts
+++ src/test/Counter.test.ts
@@ -1,8 +1,8 @@
-import { describe, test, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/svelte";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
-import Counter from "../lib/Counter.svelte";
+import { describe, test, expect, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import Counter from '../lib/Counter.svelte';
 
 const user = userEvent.setup();
 
@@ -10,63 +10,63 @@ beforeEach(() => {
   render(Counter, { count: 0 });
 });
 
-test("should render correctly", () => {
+test('should render correctly', () => {
   expect(screen.getByText(/count is/i)).toBeInTheDocument();
   expect(screen.getByText(/click edit/i)).toBeInTheDocument();
 });
 
-test("should be able to increment", async () => {
+test('should be able to increment', async () => {
   await user.click(screen.getByText(/increment/i));
 
-  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 1");
+  expect(screen.getByText(/count/i).innerHTML).toBe('Count is 1');
 });
 
-test("should be able to decrement", async () => {
+test('should be able to decrement', async () => {
   await user.click(screen.getByText(/decrement/i));
 
-  expect(screen.getByText(/count/i).innerHTML).toBe("Count is -1");
+  expect(screen.getByText(/count/i).innerHTML).toBe('Count is -1');
 });
 
-test("should be able to reset", async () => {
+test('should be able to reset', async () => {
   await user.click(screen.getByText(/increment/i));
   await user.click(screen.getByText(/increment/i));
 
-  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 2");
+  expect(screen.getByText(/count/i).innerHTML).toBe('Count is 2');
 
   await user.click(screen.getByText(/reset/i));
 
-  expect(screen.getByText(/count/i).innerHTML).toBe("Count is 0");
+  expect(screen.getByText(/count/i).innerHTML).toBe('Count is 0');
 });
 
-test("should be able to edit title", async () => {
-  await user.click(screen.getByText("Edit"));
-  await user.type(screen.getByPlaceholderText("Title"), "New Title");
+test('should be able to edit title', async () => {
+  await user.click(screen.getByText('Edit'));
+  await user.type(screen.getByPlaceholderText('Title'), 'New Title');
   await user.click(screen.getByText(/reset/i));
 
-  expect(screen.getByText("New Title")).toBeInTheDocument();
+  expect(screen.getByText('New Title')).toBeInTheDocument();
 
-  await user.click(screen.getByText("Edit"));
-  await user.clear(screen.getByPlaceholderText("Title"));
-  await user.type(screen.getByPlaceholderText("Title"), "Another Title");
+  await user.click(screen.getByText('Edit'));
+  await user.clear(screen.getByPlaceholderText('Title'));
+  await user.type(screen.getByPlaceholderText('Title'), 'Another Title');
   await user.click(screen.getByText(/reset/i));
 
-  expect(screen.getByText("Another Title")).toBeInTheDocument();
+  expect(screen.getByText('Another Title')).toBeInTheDocument();
 });
 
 test('should handle cancel edit', async () => {
-  await user.click(screen.getByText("Edit"));
-  await user.type(screen.getByPlaceholderText("Title"), "New Title");
-  await user.click(screen.getByText("Cancel"));
+  await user.click(screen.getByText('Edit'));
+  await user.type(screen.getByPlaceholderText('Title'), 'New Title');
+  await user.click(screen.getByText('Cancel'));
 
-  expect(screen.queryByText("New Title")).not.toBeInTheDocument();
-})
+  expect(screen.queryByText('New Title')).not.toBeInTheDocument();
+});
 
 test('should handle keydown', async () => {
-  await user.click(screen.getByText("Edit"));
-  await user.type(screen.getByPlaceholderText("Title"), "New Title");
+  await user.click(screen.getByText('Edit'));
+  await user.type(screen.getByPlaceholderText('Title'), 'New Title');
   await user.keyboard('{Enter}');
 
   screen.debug();
 
-  expect(screen.getByText("New Title")).toBeInTheDocument();
-})
+  expect(screen.getByText('New Title')).toBeInTheDocument();
+});
